c3f9b2976a3561e8c0298a4594730d7b9c7fe02d
ibraim <ing.gonzalezvij@hotmail.com>
2025-10-15 13:50:57 -0600

    puntos 2 y 3 agregados

---
+++ main.js
@@ -0,0 +1,139 @@
+
+let productos = [
+  {
+    nombre: "Helado de Vainilla",
+    sabor: "Vainilla",
+    precio: 10,
+    stock: 50,
+    categoria: "Clásicos",
+    estado: "activo",
+  },
+  {
+    nombre: "Helado de Chocolate",
+    sabor: "Chocolate",
+    precio: 15,
+    stock: 30,
+    categoria: "Clásicos",
+    estado: "activo",
+  },
+    {
+      nombre: "Helado de Fresa",
+      sabor: "Fresa",
+      precio: 3,
+      stock: 25,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Menta con Chispas",
+      sabor: "Menta con Chispas",
+      precio: 20,
+      stock: 20,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Cookies and Cream",
+      sabor: "Cookies and Cream",
+      precio: 14,
+      stock: 15,
+      categoria: "Clásicos",
+      estado: "descontinuado",
+    },
+    {
+      nombre: "Helado de Mango",
+      sabor: "Mango",
+      precio: 4,
+      stock: 10,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Pistacho",
+      sabor: "Pistacho",
+        precio: 4.8,
+      stock: 5,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Dulce de Leche",
+      sabor: "Dulce de Leche",
+      precio: 17,
+      stock: 12,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Limón",
+      sabor: "Limón",
+      precio: 10,
+      stock: 8,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Café",
+      sabor: "Café",
+      precio: 5,
+      stock: 12,
+      categoria: "Clásicos",
+      estado: "descontinuado",
+    },
+    {
+      nombre: "Helado de Avellana",
+      sabor: "Avellana",
+      precio: 19,
+      stock: 10,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+      nombre: "Helado de Coco",
+      sabor: "Coco",
+      precio: 6,
+      stock: 20,
+      categoria: "Clásicos",
+      estado: "activo",
+    },
+    {
+        nombre: "Helado de Frambuesa",
+        sabor: "Frambuesa",
+        precio: 4,
+        stock: 18,
+        categoria: "Clásicos",
+        estado: "activo",
+      },
+        {
+          nombre: "Helado de Té Verde",
+          sabor: "Té Verde",
+          precio: 9,
+          stock: 10,
+          categoria: "Clásicos",
+          estado: "descontinuado",
+        },
+        {
+          nombre: "Helado de Caramelo Salado",
+          sabor: "Caramelo Salado",
+          precio: 11,
+          stock: 10,
+          categoria: "Clásicos",
+          estado: "descontinuado",
+        },
+];
+
+/*  2. Mostrar todos los productos en consola usando forEach.
+*/
+productos.forEach((producto) => {
+    console.log(producto);
+})
+
+/* ---filtro de productos con precio mayor a un valor definido------ */
+let productosCaros = productos.filter((producto) => producto.precio > 10);
+console.log("Productos caros:", productosCaros);
+/* ---filtro de productos con stock bajo (menor a 10 unidades)------ */
+let productosStockBajo = productos.filter((producto) => producto.stock < 10);
+console.log("Productos con stock bajo:", productosStockBajo);
+/* ---filtro de productos activos------ */
+let productosActivos = productos.filter((producto) => producto.estado === "activo");
+console.log("Productos activos:", productosActivos);
